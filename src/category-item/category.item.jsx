f9d45c38c8c68ccd1bd5f4bc90a7d49901046f2a
kenken509 <ariesllesis050382@gmail.com>
2022-10-03 10:26:22 +0800

    first commit

---
+++ src/category-item/category.item.jsx
@@ -0,0 +1,20 @@
+import './category.item.styles.scss';
+
+const CategoryItem = (props) => {
+  const { title, imageUrl } = props.category;
+  return (
+    <div className="category-container">
+      <img
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        alt={title}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
